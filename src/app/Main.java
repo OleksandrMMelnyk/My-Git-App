57a5ed0d8a17c8541fd0e80fbdb9dc904c593d89
OleksandrMMelnyk <melnyk.oleksandr.m@gmail.com>
2023-11-09 14:49:04 +0200

    Initial commit

--- src/app/Main.java
+++ src/app/Main.java
@@ -4,6 +4,5 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("My git app for Users.");
         System.out.println("The informative application.");
-        System.out.println("The informative application");
     }
 }
